8bfddf60fcb525df2549525efc9e9fee40fc4c34
Dan <danielgrinkevich@gmail.com>
2015-08-19 23:01:17 -0400

    updated fw rules, fixed nodog rule, possibly fixed passwd/shadow

--- packages/qmp-nycmesh/files/etc/nycmesh/nycmesh_conf.sh
+++ packages/qmp-nycmesh/files/etc/nycmesh/nycmesh_conf.sh
@@ -43,7 +43,7 @@ uci set network.lan.netmask='255.255.255.0'
 uci set nodogsplash.@instance[0].enabled='1'
 uci set nodogsplash.@instance[0].gatewayname='NYC Mesh Splash'
 uci del_list nodogsplash.@instance[0].authenticated_users='block to 10.0.0.0/8'
-uci del_list nodogsplash.@instance[0].authenticated_users='block to 172.16.0.0/12'
+uci add_list nodogsplash.@instance[0].authenticated_users='block to 172.16.0.0/12'
 uci add_list nodogsplash.@instance[0].authenticated_users='allow all to 10.0.0.0/8'
 
 #temporary until we decide what ports
@@ -73,10 +73,18 @@ echo "qm6 mesh6" > /etc/mdns/domain6
 (crontab -l ; echo "* * * * * /etc/nycmesh/tinc_putkey.sh")| crontab -
 
 #firewall rules
-echo "#block access to internal lan" >> /etc/firewall.user
+echo "#block access to private address space" >> /etc/firewall.user
 echo "iptables -I FORWARD -d 192.168.0.0/16 -j DROP" >> /etc/firewall.user
 echo "iptables -I FORWARD -d 172.16.0.0/12 -j DROP" >> /etc/firewall.user
 echo "iptables -I FORWARD -o eth+ -d 10.0.0.0/8 -j DROP" >> /etc/firewall.user
+
+echo "#allow only SSH and tinc on WAN" >> /etc/firewall.user
+echo 'wan=$(uci get qmp.interfaces.wan_devices)' >> /etc/firewall.user
+echo 'iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT' >> /etc/firewall.user
+echo 'iptables -A INPUT -p tcp --dport 22 -j ACCEPT -i $wan' >> /etc/firewall.user
+echo 'iptables -A INPUT -p tcp --dport 655 -j ACCEPT -i $wan' >> /etc/firewall.user
+echo 'iptables -A INPUT -p udp --dport 655 -j ACCEPT -i $wan' >> /etc/firewall.user
+echo 'iptables -A INPUT -j DROP -i $wan' >> /etc/firewall.user
 
 #keep /etc/nycmesh during upgrades
 echo "/etc/nycmesh" >> /etc/sysupgrade.conf
